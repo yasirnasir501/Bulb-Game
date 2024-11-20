b62719ef9e14341f5633e532a26d78cc659df0bb
yasirnasir501 <yasirnasir501@gmail.com>
2024-11-20 22:16:31 +0500

    BULB ON & OFF using javascript

---
+++ App.js
@@ -0,0 +1,11 @@
+let bulbimg = document.getElementById('bulbIMG')
+
+function bulbon(){
+    bulbimg.src = './bulbON.png'
+    document.getElementById('btn1').style.background = 'rgb(236, 71, 0)';
+}
+
+function bulboff(){
+    bulbimg.src = './bulbOFF.png';
+    document.getElementById('btn1').style.background = 'rgb(236, 71, 0)';
+}
